9aeccc9b77c093401078b8cb61364faaf23691ed
James Lefrère <jlefrere@gmail.com>
2015-01-12 02:26:27 +0100

    Fix package.js dependencies

--- package.js
+++ package.js
@@ -7,19 +7,24 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@1.0");
+  api.use("http", "server");
   api.use([
     "coffeescript",
-    "accounts-base",
-    "accounts-oauth2-helper",
-    "http"
+    "oauth2",
+    "oauth",
+    "underscore",
+    "service-configuration"
     ], ["client", "server"]);
-  api.use("templating", "client");
+  api.use([
+    "templating",
+    "random"
+    ], "client");
   api.addFiles("twitch_globals.js", ["client", "server"]);
   api.addFiles([
     "twitch_client.coffee",
-    "twitch_configure.coffee",
-    "twitch_configure.html"
+    "twitch_configure.html",
+    "twitch_configure.coffee"
     ], "client");
-  api.addFiles("twitch_common.coffee", ["client", "server"]);
+  api.export("Twitch");
   api.addFiles("twitch_server.coffee", "server");
 });
